3b554ca763d459a71fd91c7cf956f1522a92504b
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-07-22 13:43:21 +0300

    added active filters in catalog

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -7,19 +7,11 @@ const Catalog = () => {
 
     const [filterPrice, setFilterPrice] = useState('');
 
-    // const [brandApple, setBrandApple] = useState(false);
-
-    // const [brandSamsung, setBrandSamsung] = useState(false);
-
-    // const [brandLg, setBrandLg] = useState(false);
-
-    // const [brandXiaomi, setBrandXiaomi] = useState(false);
-
-    const [filterBrands,setFilterBrands] = useState({
-        appleBrand:false,
-        samsungBrand:false,
-        lgBrand:false,
-        xiaomiBrand:false,
+    const [filterBrands, setFilterBrands] = useState({
+        appleBrand: false,
+        samsungBrand: false,
+        lgBrand: false,
+        xiaomiBrand: false,
     })
 
     const mainCatalogRef = useRef(null);
@@ -46,89 +38,139 @@ const Catalog = () => {
                             <div className="filterBar__categories">
                                 <label className="filterBar__categories-item" onClick={() => setFilterCategories('Electronics Devices')}>
                                     <input name="radio" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterCategories === 'Electronics Devices' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterCategories === 'Electronics Devices' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Electronics Devices</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterCategories('Computer & Laptop')}>
                                     <input name="radio" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterCategories === 'Computer & Laptop' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterCategories === 'Computer & Laptop' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Computer & Laptop</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterCategories('Computer Accessories')}>
                                     <input name="radio" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterCategories === 'Computer Accessories' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterCategories === 'Computer Accessories' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Computer Accessories</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterCategories('SmartPhone')}>
                                     <input name="radio" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterCategories === 'SmartPhone' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterCategories === 'SmartPhone' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>SmartPhone</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterCategories('Headphones')}>
                                     <input name="radio" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterCategories === 'Headphones' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterCategories === 'Headphones' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Headphones</p>
                                 </label>
                             </div>
                             <div className="filterBar__price">
                                 <h1 className="filterBar__title">Price</h1>
-                                <label className="filterBar__categories-item" onClick={() => setFilterPrice('All Price')}>
-                                    <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
-                                    <p className={filterPrice === 'All Price' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>All Price</p>
-                                </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterPrice('Under $20')}>
                                     <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterPrice === 'Under $20' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterPrice === 'Under $20' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Under $20</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterPrice('$20 to $100')}>
                                     <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterPrice === '$20 to $100' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterPrice === '$20 to $100' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>$20 to $100</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterPrice('$100 to $500')}>
                                     <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterPrice === '$100 to $500' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterPrice === '$100 to $500' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>$100 to $500</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterPrice('$500 to $1,000')}>
                                     <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterPrice === '$500 to $1,000' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterPrice === '$500 to $1,000' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>$500 to $1,000</p>
                                 </label>
                                 <label className="filterBar__categories-item" onClick={() => setFilterPrice('Above $1,000')}>
                                     <input name="rd__price" type="radio" className="categories__item-radio" />
-                                    <span className="categories__item-radioStyle"></span>
+                                    <span className={filterPrice === 'Above $1,000' ? "categories__item-radioStyle categories__item-radioStyleActive" : "categories__item-radioStyle"}></span>
                                     <p className={filterPrice === 'Above $1,000' ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Above $1,000</p>
                                 </label>
                             </div>
                             <div className="filterBar__price">
                                 <h1 className="filterBar__title">POPULAR BRANDS</h1>
                                 <label className="filterBar__categories-item" >
-                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({...prev,appleBrand:!prev.appleBrand}))} />
-                                    <span className="categories__item-checkBoxStyle"></span>
+                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({ ...prev, appleBrand: !prev.appleBrand }))} />
+                                    <span className={filterBrands.appleBrand ? "categories__item-checkBoxStyle categories__item-checkBoxStyleActive" : "categories__item-checkBoxStyle"}></span>
                                     <p className={filterBrands.appleBrand ? "categories__item-text categories__item-textActive" : "categories__item-text"} >Apple</p>
                                 </label>
                                 <label className="filterBar__categories-item"  >
-                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({...prev,samsungBrand:!prev.samsungBrand}))} />
-                                    <span className="categories__item-checkBoxStyle"></span>
+                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({ ...prev, samsungBrand: !prev.samsungBrand }))} />
+                                    <span className={filterBrands.samsungBrand ? "categories__item-checkBoxStyle categories__item-checkBoxStyleActive" : "categories__item-checkBoxStyle"}></span>
                                     <p className={filterBrands.samsungBrand ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Samsung</p>
                                 </label>
                                 <label className="filterBar__categories-item" >
-                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({...prev,lgBrand:!prev.lgBrand}))}/>
-                                    <span className="categories__item-checkBoxStyle"></span>
+                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({ ...prev, lgBrand: !prev.lgBrand }))} />
+                                    <span className={filterBrands.lgBrand ? "categories__item-checkBoxStyle categories__item-checkBoxStyleActive" : "categories__item-checkBoxStyle"}></span>
                                     <p className={filterBrands.lgBrand ? "categories__item-text categories__item-textActive" : "categories__item-text"}>LG</p>
                                 </label>
                                 <label className="filterBar__categories-item" >
-                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({...prev,xiaomiBrand:!prev.xiaomiBrand}))} />
-                                    <span className="categories__item-checkBoxStyle"></span>
+                                    <input type="checkbox" className="categories__item-radio" onClick={() => setFilterBrands((prev) => ({ ...prev, xiaomiBrand: !prev.xiaomiBrand }))} />
+                                    <span className={filterBrands.xiaomiBrand ? "categories__item-checkBoxStyle categories__item-checkBoxStyleActive" : "categories__item-checkBoxStyle"}></span>
                                     <p className={filterBrands.xiaomiBrand ? "categories__item-text categories__item-textActive" : "categories__item-text"}>Xiaomi</p>
                                 </label>
                             </div>
                         </div>
                         <div className="sectionCatalog__productsBlock">
-                            products
+                            <div className="productsBlock__top">
+                                <input type="text" className="productsBlock__top-input" placeholder="Search for anything..." />
+                                <img src="/images/sectionCatalog/glass.png" alt="" className="productsBlock__top-img" />
+                            </div>
+                            <div className="productsBlock__top-filters">
+                                <div className="top__filters-left">
+                                    <p className="filters__left-text">Active Filters:</p>
+
+                                    {filterCategories !== '' &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">{filterCategories}</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterCategories('')} />
+                                        </div>
+                                    }
+
+                                    {filterPrice !== '' &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">{filterPrice}</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterPrice('')}/>
+                                        </div>
+                                    }
+
+                                    {filterBrands.appleBrand &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">Apple</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterBrands((prev)=>({...prev,appleBrand:false}))}/>
+                                        </div>
+                                    }
+
+                                    {filterBrands.samsungBrand &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">Samsung</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterBrands((prev)=>({...prev,samsungBrand:false}))}/>
+                                        </div>
+                                    }
+
+                                    {filterBrands.lgBrand &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">LG</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterBrands((prev)=>({...prev,lgBrand:false}))}/>
+                                        </div>
+                                    }
+
+                                    {filterBrands.xiaomiBrand &&
+                                        <div className="filters__left-filterItem">
+                                            <p className="filterItem__text">Xiaomi</p>
+                                            <img src="/images/sectionCatalog/cross.png" alt="" className="filterItem-img" onClick={() => setFilterBrands((prev)=>({...prev,xiaomiBrand:false}))}/>
+                                        </div>
+                                    }
+
+                                </div>
+                                <div className="top__filters-right">
+                                    <p className="filters__right-text">10</p>
+                                    <p className="filters__right-desc">Results found.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
