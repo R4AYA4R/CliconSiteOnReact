267b1c595924c3a115aa7ea5fc452dd8150e408e
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-09-06 11:18:07 +0300

    deleted checkAuth from formPage

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -90,32 +90,6 @@ const FormPage = () => {
         }
     }
 
-    // функция для проверки авторизован ли пользователь
-    const checkAuth = async () => {
-
-        setLoadingUser(true); // изменяем поле isLoading состояния пользователя в userSlice на true(то есть пошла загрузка)
-
-        // оборачиваем в try catch,чтобы отлавливать ошибки
-        try{
-
-            // здесь используем уже обычный axios,указываем тип в generic,что в ответе от сервера ожидаем наш тип данных AuthResponse,указываем наш url до нашего роутера(/api) на бэкэнде(API_URL мы импортировали из другого нашего файла) и через / указываем refresh(это тот url,где мы выдаем access и refresh токены на бэкэнде),и вторым параметром указываем объект опций,указываем поле withCredentials true(чтобы автоматически с запросом отправлялись cookies)
-            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`,{withCredentials:true});
-
-            console.log(response);
-
-            checkAuthUser(response.data); // вызываем нашу функцию(action) для изменения состояния пользователя и передаем туда response.data(в данном случае это объект с полями accessToken,refreshToken и user,которые пришли от сервера)
-
-        }catch(e:any){
-
-            console.log(e.response?.data?.message); // если была ошибка,то выводим ее в логи,берем ее из ответа от сервера  из поля message из поля data у response у e
-
-        }finally{
-            // в блоке finally будет выполнен код в независимости от try catch(то есть в любом случае,даже если будет ошибка)
-            setLoadingUser(false); // изменяем поле isLoading состояния пользователя в userSlice на false(то есть загрузка закончена)
-        }
-
-    }
-
 
     return (
         <main className={onScreen.sectionMainFormPageIntersecting ? "main mainCatalog mainCatalog__active" : "main mainCatalog"} ref={mainFormPageRef} id="formPage">
