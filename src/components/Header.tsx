d5964f27549ff11c44a7cbf10ba860e4295d17ed
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-10-02 13:34:02 +0300

    added some mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,8 @@ import AuthService from "../services/AuthService";
 
 const Header = () => {
 
+    const [activeMobileMenu,setActiveMobileMenu] = useState(false);
+
     const {data} = apiBasket.useGetAllProductsBasketQuery(null);
 
     const {checkAuthUser,setLoadingUser,setAuthUser} = useActions(); // берем actions для изменения состояния пользователя у слайса(редьюсера) userSlice у нашего хука useActions уже обернутые в диспатч,так как мы оборачивали это в самом хуке useActions
@@ -48,6 +50,38 @@ const Header = () => {
 
                         </li>
                     </ul>
+
+                    <ul className={activeMobileMenu ? "header__menuMobileList header__menuMobileListActive" : "header__menuMobileList"}>
+                        <li className="menuList__item">
+                            <NavLink to="/" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>Home</NavLink>
+                        </li>
+                        <li className="menuList__item">
+                            <NavLink to="/catalog" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>Catalog</NavLink>
+                        </li>
+                        <li className="menuList__item">
+                            <NavLink to="/about" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>About Us</NavLink>
+                        </li>
+                        <li className="menuList__item menuList__item-cart">
+                            <NavLink to="/cart" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkCartActive" : "menuList__item-link"}>
+                                <img src="/images/header/ShoppingCartSimple.png" alt="" className="item__link-img" />
+                                <span className="item__link-spanCart">{data?.length}</span>
+                            </NavLink>
+                        </li>
+                        <li className="menuList__item menuList__item-cart">
+                            <NavLink to='/user' className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>
+                                <img src="/images/header/User.png" alt="" className="item__link-img" />
+                            </NavLink>
+
+                        </li>
+                    </ul>
+
+                    <button className="header__menuBtn" onClick={()=>setActiveMobileMenu((prev)=>!prev)}>
+                        <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveScale" : "menuBtn__span"}></span>
+                        <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveRotate2" : "menuBtn__span"}></span>
+                        <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveRotate3" : "menuBtn__span"}></span>
+                        <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveScale" : "menuBtn__span"}></span>
+                    </button>
+
                 </div>
             </div>
         </header>
