c0a90cb092388b4ba6bdbb810219a450c5a373a2
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-10-03 12:55:04 +0300

    added adaptive mobile menu and full adaptive homePage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,13 @@ import AuthService from "../services/AuthService";
 
 const Header = () => {
 
-    const [activeMobileMenu,setActiveMobileMenu] = useState(false);
+    const [activeMobileMenu, setActiveMobileMenu] = useState(false);
 
-    const {data} = apiBasket.useGetAllProductsBasketQuery(null);
+    const { data } = apiBasket.useGetAllProductsBasketQuery(null);
 
-    const {checkAuthUser,setLoadingUser,setAuthUser} = useActions(); // берем actions для изменения состояния пользователя у слайса(редьюсера) userSlice у нашего хука useActions уже обернутые в диспатч,так как мы оборачивали это в самом хуке useActions
+    const { checkAuthUser, setLoadingUser, setAuthUser } = useActions(); // берем actions для изменения состояния пользователя у слайса(редьюсера) userSlice у нашего хука useActions уже обернутые в диспатч,так как мы оборачивали это в самом хуке useActions
 
-    const {isAuth} = useTypedSelector(state => state.userSlice);  // указываем наш слайс(редьюсер) под названием userSlice и деструктуризируем у него поле состояния isAuth для проверки,авторизован ли пользователь,используя наш типизированный хук для useSelector
+    const { isAuth } = useTypedSelector(state => state.userSlice);  // указываем наш слайс(редьюсер) под названием userSlice и деструктуризируем у него поле состояния isAuth для проверки,авторизован ли пользователь,используя наш типизированный хук для useSelector
 
 
     return (
@@ -61,21 +61,26 @@ const Header = () => {
                         <li className="menuList__item">
                             <NavLink to="/about" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>About Us</NavLink>
                         </li>
-                        <li className="menuList__item menuList__item-cart">
-                            <NavLink to="/cart" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkCartActive" : "menuList__item-link"}>
-                                <img src="/images/header/ShoppingCartSimple.png" alt="" className="item__link-img" />
-                                <span className="item__link-spanCart">{data?.length}</span>
-                            </NavLink>
-                        </li>
-                        <li className="menuList__item menuList__item-cart">
-                            <NavLink to='/user' className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>
-                                <img src="/images/header/User.png" alt="" className="item__link-img" />
-                            </NavLink>
 
-                        </li>
+                        <div className="menuMobileList__userCartBlock">
+                            <li className="menuList__item menuList__item-cart">
+                                <NavLink to="/cart" className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkCartActive" : "menuList__item-link"}>
+                                    <img src="/images/header/ShoppingCartSimple.png" alt="" className="item__link-img" />
+                                    <span className="item__link-spanCart">{data?.length}</span>
+                                </NavLink>
+                            </li>
+                            <li className="menuList__item menuList__item-cart">
+                                <NavLink to='/user' className={({ isActive }) => isActive ? "menuList__item-link menuList__item-linkActive" : "menuList__item-link"}>
+                                    <img src="/images/header/User.png" alt="" className="item__link-img" />
+                                </NavLink>
+
+                            </li>
+                        </div>
+
+
                     </ul>
 
-                    <button className="header__menuBtn" onClick={()=>setActiveMobileMenu((prev)=>!prev)}>
+                    <button className="header__menuBtn" onClick={() => setActiveMobileMenu((prev) => !prev)}>
                         <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveScale" : "menuBtn__span"}></span>
                         <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveRotate2" : "menuBtn__span"}></span>
                         <span className={activeMobileMenu ? "menuBtn__span menuBtn__spanActiveRotate3" : "menuBtn__span"}></span>
